5392a92a483dc87e60345b599e8995d8fcc151e0
nchkss <nichickevich@gmail.com>
2023-02-21 15:59:21 +0200

    refactor the code base, resuming work with this project

--- js/index.js
+++ js/index.js
@@ -1,24 +1,43 @@
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const pagesInput = document.getElementById('pages');
-const readInput = document.getElementById('read');
+/* eslint-disable no-return-assign */
+/* eslint-disable consistent-return */
+/* eslint-disable no-param-reassign */
 const submitBtn = document.getElementById('submit');
-
+const inputs = Array.from(document.querySelectorAll('input[type=text]'));
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+const testBook = new Book('Harry Potter', 'Joann Rowling', '692', 'yes');
+myLibrary.push(testBook);
+
+function emptyFieldsChecker() {
+  const errMsg = document.querySelector('.inputs');
+  if (inputs.map((field) => field.value.length === 0).includes(true)) {
+    errMsg.classList.add('error');
+    return false;
+  }
+  errMsg.classList.remove('error');
+  return true;
 }
 
 function addBookToLibrary() {
-  const book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.value);
-  myLibrary.push(book);
+  if (!emptyFieldsChecker()) {
+    return;
+  }
+  const newBook = new Book(...inputs.map((field) => field.value));
+  myLibrary.push(newBook);
+  inputs.forEach((field) => field.value = '');
 }
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', (e) => {
   addBookToLibrary();
+  e.preventDefault();
   console.log(myLibrary);
 });
